ab1610ca5dd619a675dfdb318ef5d319c276e379
acid_slug <acid_slug@protonmail.com>
2023-12-05 23:53:00 -0500

    Day5 - Part 1

--- day5/Program.cs
+++ day5/Program.cs
@@ -1,2 +1,170 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+
+internal class Program {
+
+    private static void Main(string[] args) {
+        var instances = AppDomain.CurrentDomain
+                .GetAssemblies()
+                .SelectMany(x => x.GetTypes())
+                .Where(x => typeof(MapInterface).IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract)
+                .Select(Activator.CreateInstance);
+
+            Dictionary<string, MapInterface> maps = instances.ToDictionary(x => ((MapInterface)x).MapIdentifier, x => (MapInterface)x);
+
+        var seeds = new List<Seed>();
+
+        ParseFile();
+        PartOne();
+        PartTwo();
+
+        
+        void PartOne() {
+            MapInterface currentMap = maps[$"seed-to-soil"];
+            foreach(var seed in seeds) {
+                var value = seed.Identifier;
+                while(currentMap.Destination != "location") {
+                    value = currentMap.GetDestination(value);
+                    currentMap = maps.Values.FirstOrDefault(map => map.Source == currentMap.Destination) ?? throw new Exception($"Could not find map with source {currentMap.Destination}");
+                }
+                seed.Location = currentMap.GetDestination(value);
+                currentMap = maps[$"seed-to-soil"];
+                Console.WriteLine($"Seed {seed.Identifier} has location {seed.Location}");
+            }
+
+            Console.WriteLine($"Nearest location is {seeds.Min(seed => seed.Location)}");
+        }
+
+        void PartTwo() {
+            
+        }
+
+        void ParseFile() {
+            var bundleName = string.Empty;
+            var bundle = new List<MapEntry>();
+            var bundleNamePattern = @$"(?<bundleName>[a-zA-X\-]+)( map)?:";
+            foreach(var line in File.ReadLines("./input.txt")) {
+                switch(line) {
+                    case string a when a.Contains("seeds:"): {
+                        foreach(var seedNumber in line.Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Skip(1).Select(long.Parse)) {
+                            seeds.Add(new Seed(seedNumber));
+                        }
+                        break;
+                    }
+                    case string a when a.Contains(":"): {
+                        bundleName = Regex.Match(line, bundleNamePattern).Groups["bundleName"].Value;
+                        break;
+                    }
+                    case string a when string.IsNullOrEmpty(a): {
+                        if(string.IsNullOrEmpty(bundleName) || bundle.Count == 0) break;
+                        DispatchBundle(bundleName, [.. bundle]);
+                        bundleName = string.Empty;
+                        bundle.Clear();
+                        break;
+                    }
+                    default: {
+                        bundle.Add(
+                            line.Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+                            .Select(long.Parse)
+                            .ToArray()
+                        );
+                        break;
+                    }
+                }
+            }
+
+            // Make sure we send last bundle if any.
+            if(string.IsNullOrEmpty(bundleName) || bundle.Count == 0) return;
+            DispatchBundle(bundleName, [.. bundle]);
+            bundleName = string.Empty;
+            bundle.Clear();
+        }
+
+        void DispatchBundle(string bundleName, MapEntry[] bundle) {
+            maps[bundleName].MapEntries = bundle;
+        }
+    }
+}
+
+public record MapEntry(long Destination, long Source, long Range) {
+    public static implicit operator MapEntry(long[] values) {
+        if(values.Length < 3) throw new Exception("Invalid array to convert to MapEntry.");
+        return new MapEntry(values[0], values[1], values[2]);
+    }
+}
+
+public abstract class MapInterface {
+    public MapEntry[] MapEntries {get; set;}
+    public abstract string Source {get;}
+    public abstract string Destination {get;}
+    public string MapIdentifier {
+        get {
+            if(string.IsNullOrEmpty(Destination)) return Source;
+            if(string.IsNullOrEmpty(Source)) return Destination;
+            return $"{Source}-to-{Destination}";
+        }
+    }
+
+    public long GetDestination(long source) {
+        // Console.Write($"{Source} value {source} maps to {Destination} value ");
+        foreach(var entry in MapEntries){
+            (long position, bool _)= (source, source >= entry.Source && source <= entry.Source + entry.Range) 
+                switch { 
+                    (_, true) => (source - entry.Source, true), 
+                    _ => (-1, false) 
+            };
+
+            if(position == -1) continue;
+            return entry.Destination + position;
+        }
+        return source;
+    }
+}
+
+public class Seed(long identifier) {
+    public long Identifier { get; init; } = identifier;
+    public long Location {get; set;}
+}
+
+public class Seed2SoilMap : MapInterface {
+
+    public override string Source => "seed";
+
+    public override string Destination => "soil";
+}
+
+public class Soil2FertilizerMap : MapInterface {
+    public override string Source => "soil";
+
+    public override string Destination => "fertilizer";
+}
+
+public class Fertilizer2WaterMap : MapInterface {
+    public override string Source => "fertilizer";
+
+    public override string Destination => "water";
+}
+
+public class Water2LightMap : MapInterface {
+    public override string Source => "water";
+
+    public override string Destination => "light";
+}
+
+public class Light2TemperatureMap : MapInterface {
+    public override string Source => "light";
+
+    public override string Destination => "temperature";
+}
+
+public class Temperature2HumidityMap : MapInterface {
+    public override string Source => "temperature";
+
+    public override string Destination => "humidity";
+}
+
+public class Humidity2LocationMap : MapInterface {
+    public override string Source => "humidity";
+
+    public override string Destination => "location";
+}
+
