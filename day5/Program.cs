76c153afb492d985f97f6aef01d700d11bc8d268
acid_slug <acid_slug@protonmail.com>
2023-12-08 01:17:05 -0500

    input.txt doesn't give the right answer but input-demo does :(

--- day5/Program.cs
+++ day5/Program.cs
@@ -2,6 +2,7 @@
 
 internal class Program {
 
+    public const string Filename = "./input-demo.txt";
     private static void Main(string[] args) {
         var instances = AppDomain.CurrentDomain
                 .GetAssemblies()
@@ -9,9 +10,10 @@ internal class Program {
                 .Where(x => typeof(MapInterface).IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract)
                 .Select(Activator.CreateInstance);
 
-            Dictionary<string, MapInterface> maps = instances.ToDictionary(x => ((MapInterface)x).MapIdentifier, x => (MapInterface)x);
+        var maps = instances.ToDictionary(x => ((MapInterface)x).MapIdentifier, x => (MapInterface)x);
 
         var seeds = new List<Seed>();
+        var seedsAsRanges = new List<(long Min, long Max)>();
 
         ParseFile();
         PartOne();
@@ -39,22 +41,61 @@ internal class Program {
         void PartTwo() {
             ParseSeedsAsRange();
             MapInterface currentMap = maps[$"seed-to-soil"];
-            foreach(var seed in seeds) {
-                var value = seed.Identifier;
-                while(currentMap.Destination != "location") {
-                    value = currentMap.GetDestination(value);
-                    currentMap = maps.Values.FirstOrDefault(map => map.Source == currentMap.Destination) ?? throw new Exception($"Could not find map with source {currentMap.Destination}");
+            var value = 0L;
+            var rangesToAnalyze = seedsAsRanges;
+            while(currentMap.Destination != "location") {
+                // Get map ranges ordered by destination values
+                Console.WriteLine($"In map {currentMap.MapIdentifier}");
+                var currentRanges = rangesToAnalyze;
+                rangesToAnalyze = currentMap.GetRanges
+                        .Select(x => 
+                            (x.SourceMin, x.SourceMax))
+                        .SelectMany(range1 => 
+                            rangesToAnalyze
+                                .Where(range2 => Math.Max(range1.SourceMin, range2.Min) < Math.Min(range1.SourceMax, range2.Max)).Select(range2 => (Math.Max(range1.SourceMin, range2.Min), Math.Min(range1.SourceMax, range2.Max)))).ToList();
+                if(rangesToAnalyze.Count == 0) {
+                    rangesToAnalyze = currentRanges;
                 }
-                seed.Location = currentMap.GetDestination(value);
-                currentMap = maps[$"seed-to-soil"];
-                Console.WriteLine($"Seed {seed.Identifier} has location {seed.Location}");
+                // Console.WriteLine($"Smallest range is from {smallestRange.DestinationMin} to {smallestRange.DestinationMax} and maps to {smallestRange.SourceMin} - {smallestRange.SourceMax}");
+                // Find smallest possible seed within that range
+                // var seedRanges = seedsAsRanges.Where(x => x.Min <= smallestRange.SourceMax && x.Max >= smallestRange.SourceMin).ToList();
+                for(var i = 0; i < rangesToAnalyze.Count; i++) {
+                    rangesToAnalyze[i] = (
+                        currentMap.GetDestination(rangesToAnalyze[i].Min),
+                        currentMap.GetDestination(rangesToAnalyze[i].Max));
+                }
+                // foreach(var seedRange in rangesToAnalyze) {
+                //     Console.WriteLine(seedRange);
+                //     seedRange.Min = currentMap.GetDestination(seedRange.Min);
+                //     // if(seedRange.Min <= smallestRange.SourceMin) {
+                //     //     value = smallestRange.SourceMin;
+                //     //     continue;
+                //     // }
+                //     // value = seedRange.Min;
+                // }
+                value = rangesToAnalyze.OrderBy(x => x.Min).First().Min;
+                // value = currentMap.GetDestination(value);
+                currentMap = maps.Values.FirstOrDefault(map => map.Source == currentMap.Destination) ?? throw new Exception($"Could not find map with source {currentMap.Destination}");
             }
+            Console.WriteLine(value);
+            // seedsAsRanges.Location = currentMap.GetDestination(value);
+            currentMap = maps[$"seed-to-soil"];
 
-            Console.WriteLine($"Nearest location is {seeds.Min(seed => seed.Location)}");
+            while(currentMap.Destination != "location") {
+                value = currentMap.GetDestination(value);
+                currentMap = maps.Values.FirstOrDefault(map => map.Source == currentMap.Destination) ?? throw new Exception($"Could not find map with source {currentMap.Destination}");
+            }
+            
+
+            Console.WriteLine($"Nearest location is {currentMap.GetDestination(value)}");
+            // Console.WriteLine($"Seed {seed.Identifier} has location {seed.Location}");
+            // }
+
+            // Console.WriteLine($"Nearest location is {seeds.Min(seed => seed.Location)}");
         }
 
         void ParseSeeds() {
-            foreach(var line in File.ReadLines("./input-demo.txt")) {
+            foreach(var line in File.ReadLines(Program.Filename)) {
                 if(line.Contains("seeds:")) {
                     foreach(var seedNumber in line.Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Skip(1).Select(long.Parse)) {
                         seeds.Add(new Seed(seedNumber));
@@ -65,18 +106,14 @@ internal class Program {
         }
 
         void ParseSeedsAsRange() {
-            foreach(var line in File.ReadLines("./input-demo.txt")) {
+            foreach(var line in File.ReadLines(Program.Filename)) {
                 if(line.Contains("seeds:")) {
                     var seedRanges = line.Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Skip(1).Select(long.Parse).ToArray();
                     
                     for(var i = 0; i < seedRanges.Length; i += 2) {
                         Console.WriteLine($"{seedRanges[i]} generate {seedRanges[i + 1]}");
-                        seeds.Add(new Seed(seedRanges[i]));
-                        // for(long j = seedRanges[i]; j < seedRanges[i] + seedRanges[i + 1]; j++) {
-                        //     seeds.Add(new Seed(j));
-                        // }
+                        seedsAsRanges.Add((seedRanges[i], seedRanges[i] + seedRanges[i + 1]));
                     }
-                    Console.WriteLine($"Seeds: {string.Join(",", seeds.Select(x=>x.Identifier))}");
                 }
             }
         }
@@ -85,7 +122,7 @@ internal class Program {
             var bundleName = string.Empty;
             var bundle = new List<MapEntry>();
             var bundleNamePattern = @$"(?<bundleName>[a-zA-X\-]+)( map)?:";
-            foreach(var line in File.ReadLines("./input-demo.txt")) {
+            foreach(var line in File.ReadLines(Program.Filename)) {
                 switch(line) {
                     case string a when a.Contains("seeds:"): {
                         foreach(var seedNumber in line.Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Skip(1).Select(long.Parse)) {
@@ -169,6 +206,15 @@ public abstract class MapInterface {
         }
         return source;
     }
+
+    public List<(long Start, long End)> GetSourceRanges => 
+        MapEntries.Select(entry => (entry.Source, entry.Source + entry.Range)).ToList();
+    public List<(long Start, long End)> GetDestinationRanges => 
+        MapEntries.Select(entry => (entry.Destination, entry.Destination + entry.Range)).ToList();
+
+    public List<(long SourceMin, long SourceMax, long DestinationMin, long DestinationMax)> GetRanges => 
+        MapEntries.Select(entry => (entry.Source, entry.Source + entry.Range, entry.Destination, entry.Destination + entry.Range)).ToList();
+
 }
 
 public class Seed(long identifier) {
